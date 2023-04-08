2aeb998c1960541f9548e8e894a9ef85ae10eacd
Mukhammadkarim's MacBook Pro <macbook@Muhammadkarims-MacBook-Air.local>
2023-04-08 20:32:52 +0500

    Updated

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // Bismillah
+
+// Selectors
+const todoInput = document.querySelector('.todo-input')
+const todoButton = document.querySelector('.todo-button')
+const todoList = document.querySelector('.todo-list')
+
+
+// Event Listeners
+todoButton.addEventListener('click', addTodo)
+
+// Functions
+function addTodo(event) {
+	// Prevent form from submitting
+	const todoDiv = document.createElement('div');
+	todoDiv.classList.add('todo')
+}
